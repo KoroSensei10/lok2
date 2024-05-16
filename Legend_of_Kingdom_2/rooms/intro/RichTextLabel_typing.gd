cbad95b4462d3e413aaee37a4d16119e88d87809
LemerleXavier <lemerle.xavier@gmail.com>
2024-05-16 15:33:02 +0200

    intro

---
+++ Legend_of_Kingdom_2/rooms/intro/RichTextLabel_typing.gd
@@ -0,0 +1,18 @@
+extends RichTextLabel
+
+# Called when the node enters the scene tree for the first time.
+
+#func _ready() -> void:
+#	$Tween.interpolate_property(
+#		self, "percent_visible",
+#		0.0, 1.0, 7.0, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT
+#	)
+#	$Tween.start()
+	
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Legend_of_Kingdom_2/rooms/intro/typing_label.gd
@@ -0,0 +1,20 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Tween.interpolate_property(
+		self, "percent_visible",
+		0.0, 1.0, 3.0, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT
+	)
+	$Tween.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
